b2228f6c266696aed753d4a57e137f739fbcbef8
paulusyeung <paulusyeung@gmail.com>
2026-08-12 04:12:57 +0800

    feat: align ship-to selection components for improved layout in AdminSupplierRecordDialog

--- JB2026.WebApp/ClientApp/src/components/forms/AdminSupplierRecordDialog.vue
+++ JB2026.WebApp/ClientApp/src/components/forms/AdminSupplierRecordDialog.vue
@@ -71,8 +71,8 @@
 
       <v-divider class="my-2" />
 
-      <v-row dense>
-        <v-col cols="12" md="6">
+      <v-row dense align="center">
+        <v-col cols="12" md="6" class="d-flex align-center">
           <v-select
             v-model="selectedShipToName"
             :items="shipToNameOptions"
@@ -80,6 +80,7 @@
             variant="outlined"
             density="compact"
             clearable
+            hide-details
           />
         </v-col>
         <v-col cols="12" md="6" class="d-flex align-center ga-2">
